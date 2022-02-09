a65d847cb8f927a959465c89e5992b72d7e90c95
Owner <garofalo.ronald@gmail.com>
2022-02-09 13:56:28 -0500

    changed about pathing

--- client/src/screens/About/About.jsx
+++ client/src/screens/About/About.jsx
@@ -2,7 +2,7 @@
 import { useNavigate } from 'react-router-dom'
 import './About.css'
 
-export default function About(){
+export default function About({profile}){
     const nav = useNavigate()
     return (
 
@@ -11,7 +11,10 @@ export default function About(){
             <p id="about-p">A <span className='violet'>Mamoo</span> is a Memory. Good Times, Tough Times, Milestones, and Reminders, a <span className='violet'>Mamoo</span> is a way to quantify important
             events in your life. <span className='violet'>Mamoos</span> are a journaling tool, a reminder tool, or just a way to keep track
             of progress towards Milestones in your life.  Whatever's important to you can be a <span className='violet'>Mamoo</span>.   </p>
-            <button onClick={()=>{nav('/signup')}}>Sign Up</button>
+            
+            {profile === null ? <button onClick={()=>{nav('/signup')}}>Sign Up ></button> : <button onClick={()=>(nav('/mymamoos'))}>Home ></button>}
+            
+            
         </div>
 
     )
